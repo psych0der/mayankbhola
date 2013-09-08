d25cf8c254faa57ca9c3cfe53299d64aae1fd5d2
psych0der <mayankbhola@gmail.com>
2013-09-09 00:47:07 +0530

    header added

---
+++ project_static/bootstrap/js/pagehilighter.js
@@ -0,0 +1,24 @@
+$(document).ready(funtion(){
+
+var str=location.href.toLowerCase();
+$(".navigation li a").each(function() {
+if (str.indexOf(this.href.toLowerCase()) > -1) {
+$("li.highlight").removeClass("highlight");
+$(this).parent().addClass("highlight");
+}
+
+$("li.highlight").parents().each(function(){
+
+if ($(this).is("li")){
+$(this).addClass("highlight");
+}
+
+
+});
+
+
+});
+
+
+
+
