f7c2d18fb311f070136bc9ca256c5dcfa55264bc
psych0der <mayankbhola@gmail.com>
2013-09-08 19:04:25 +0530

    footer stable

---
+++ mayankbhola/common/sidebar.py
@@ -0,0 +1,38 @@
+import time
+from blogengine.models import Post,Category,Tag
+from calendar import month_name
+
+def monthList():
+    """Make a list of months to show archive links."""
+
+    if not Post.objects.count(): return []
+
+    # set up vars
+    year, month = time.localtime()[:2]
+    first = Post.objects.order_by("pub_date")[0]
+    fyear = first.pub_date.year
+    fmonth = first.pub_date.month
+    months = []
+
+    # loop over years and months
+    for y in range(year, fyear-1, -1):
+        start, end = 12, 0
+        if y == year: start = month
+        if y == fyear: end = fmonth-1
+
+        for m in range(start, end, -1):
+            months.append((y, m, month_name[m]))
+    return months
+
+
+
+def categoryList():
+    if not Category.objects.count() : return []
+    categories = Category.objects.all()
+    return categories
+
+
+def TagList():
+    if not Tag.objects.count() : return []
+    tags = Tag.objects.all()
+    return tags
